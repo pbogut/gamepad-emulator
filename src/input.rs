320710139a17abe47b1c2468ae81e3de906f7afd
Pawel Bogut <pbogut@pbogut.me>
2023-11-16 19:22:48 +0100

    WIP

--- src/input.rs
+++ src/input.rs
@@ -1,4 +1,5 @@
 mod error;
+mod mouse;
 
 pub use error::Error;
 
--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,48 @@ fn main() -> Result<(), Error> {
     let joy = joystick::Joystick::new()?;
     eprintln!("Device created: {:?}", joy.device_path()?);
 
+    // std::thread::sleep(std::time::Duration::from_secs(5));
+
+    // joy.button_press(joystick::Button::RightSouth, true)?;
+    // joy.synchronise()?;
+    // std::thread::sleep(std::time::Duration::from_secs(1));
+    // joy.button_press(joystick::Button::RightSouth, false)?;
+    // joy.synchronise()?;
+    // std::thread::sleep(std::time::Duration::from_secs(1));
+
+    // joy.button_press(joystick::Button::RightEast, true)?;
+    // joy.synchronise()?;
+    // std::thread::sleep(std::time::Duration::from_secs(1));
+    // joy.button_press(joystick::Button::RightEast, false)?;
+    // joy.synchronise()?;
+    // std::thread::sleep(std::time::Duration::from_secs(1));
+
+    // joy.button_press(joystick::Button::RightWest, true)?;
+    // joy.synchronise()?;
+    // std::thread::sleep(std::time::Duration::from_secs(1));
+    // joy.button_press(joystick::Button::RightWest, false)?;
+    // joy.synchronise()?;
+    // std::thread::sleep(std::time::Duration::from_secs(1));
+
+    // joy.button_press(joystick::Button::RightNorth, true)?;
+    // joy.synchronise()?;
+    // std::thread::sleep(std::time::Duration::from_secs(1));
+    // joy.button_press(joystick::Button::RightNorth, false)?;
+    // joy.synchronise()?;
+    // std::thread::sleep(std::time::Duration::from_secs(1));
+
+    // joy.button_press(joystick::Button::RightSouth, false)?;
+    // joy.synchronise()?;
+
+    // joy.move_axis(joystick::Axis::X, 500)?;
+    // joy.synchronise()?;
+    // std::thread::sleep(std::time::Duration::from_secs(1));
+    // joy.move_axis(joystick::Axis::X, 250)?;
+    // joy.synchronise()?;
+    // std::thread::sleep(std::time::Duration::from_secs(1));
+
+    // let rx = miceevent::listen()?;
+    // let (tx, rx) = std::sync::mpsc::channel();
     let rx_mice = MiceEvent::listen()?;
 
     let mut queue_x = queue::Queue::new();
